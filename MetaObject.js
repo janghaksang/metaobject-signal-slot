31e822d74d594d0cb469982cf38266ea4eea0d0c
janghaksang <janghaksang@gmail.com>
2018-04-21 17:01:41 +0900

    new version

--- MetaObject.js
+++ MetaObject.js
@@ -1,98 +1,98 @@
-var MetaObject = (function() {
-  var insertIndex = (sender,signalName,receiver,slotName) => {
-    if(!sender || !sender['constructor']) throw 'No sender';
-    if(!receiver || !receiver['constructor']) throw 'No receiver';
-    let senderName = sender.constructor.name;
-    let receiverName = receiver.constructor.name;
-    let metaObject = sender['metaObject'] = sender['metaObject'] || {};
-    metaObject[senderName] = metaObject[senderName] || [];
-    let signalIndex = metaObject[senderName].length;
-    let slotIndex = 0;
-    for(let i = metaObject[senderName].length-1; i >= 0; --i) {
-      if(!metaObject[senderName][i]['sender']) continue;
-      if(metaObject[senderName][i]['sender'] != senderName) continue;
-      if(!metaObject[senderName][i][signalName]) continue;
-      signalIndex = i;
-      slotIndex = metaObject[senderName][i][signalName].length;
-      for(let j = metaObject[senderName][i][signalName].length-1; j >= 0; --j) {
-        if(!metaObject[senderName][i][signalName][j]['receiver']) continue;
-        if(metaObject[senderName][i][signalName][j]['receiver'] != receiverName) continue;
-        slotIndex = j;
-        return [signalIndex,slotIndex];
-      }
-      return [signalIndex,slotIndex];
-    }
-    return [signalIndex,slotIndex];
+export default class MetaObject {
+  constructor() {
+    this.metaSignals = {};
   }
 
-  var disconnectInner = (sender,signalName,receiver,slotName) => {
-    if(!sender || !sender['constructor']) throw 'No sender';
-    if(!receiver || !receiver['constructor']) throw 'No receiver';
+  checkArgs(sender,signalName,receiver,slotName) {
+    if(!sender || !sender['constructor']) throw new Error(`No Sender: (${sender},${signalName},${receiver},${slotName})`);
+    if(typeof sender[signalName] != 'function') throw new Error(`No Signal: (${sender},${signalName},${receiver},${slotName})`);
+    if(!receiver || !receiver['constructor']) throw new Error(`No Receiver: (${sender},${signalName},${receiver},${slotName})`);
+    if(typeof receiver[slotName] != 'function') throw new Error(`No Signal: (${sender},${signalName},${receiver},${slotName})`);
+  }
+
+  init(sender,signalName,receiver,slotName) {
     let senderName = sender.constructor.name;
     let receiverName = receiver.constructor.name;
-    if(typeof sender[signalName] != 'function') throw 'No signal';
-    let metaObject = sender['metaObject'] = sender['metaObject'] || {};
-    metaObject[senderName] = metaObject[senderName] || [];
-    for(let i = metaObject[senderName].length-1; i >= 0; --i) {
-      if(!metaObject[senderName][i]['sender']) continue;
-      if(metaObject[senderName][i]['sender'] != senderName) continue;
-      if(!metaObject[senderName][i][signalName]) continue;
-      for(let j = metaObject[senderName][i][signalName].length-1; j >= 0; --j) {
-        if(!metaObject[senderName][i][signalName][j]['receiver']) continue;
-        if(metaObject[senderName][i][signalName][j]['receiver'] != receiverName) continue;
-        delete metaObject[senderName][i][signalName][j];
-        metaObject[senderName][i][signalName] = metaObject[senderName][i][signalName].filter(function(n){ return n != undefined });
-        if(metaObject[senderName][i][signalName] && metaObject[senderName][i][signalName].length) {
-          let method = sender[signalName]['signal'] || sender[signalName];
-          sender[signalName] = (...args) => {
-            method.call(receiver,...args);
-            metaObject[senderName][i][signalName].map((m)=>m.call(receiver,...args));
-          };
-          sender[signalName]['signal'] = method;
-        }
-        return;
+
+    let metaSignals = sender['metaSignals'] = sender['metaSignals'] || {};
+    metaSignals[senderName] = metaSignals[senderName] || {};
+    metaSignals[senderName][signalName] = metaSignals[senderName][signalName] || [];
+
+    metaSignals[senderName][signalName].push({
+      receiver: receiver,
+      receiverName: receiverName,
+      slotName: slotName,
+      method: function (...args) { return receiver[slotName].call(receiver,...args); }
+    });
+  }
+
+  deinit(sender,signalName,receiver,slotName) {
+    let senderName = sender.constructor.name;
+    let receiverName = receiver.constructor.name;
+
+    let metaSignals = sender['metaSignals'];
+    metaSignals[senderName] = metaSignals[senderName] || {};
+    metaSignals[senderName][signalName] = metaSignals[senderName][signalName] || [];
+
+    let removed = metaSignals[senderName][signalName].filter(c=>{
+      if(c.receiver===receiver&&c.receiverName==receiverName&&c.slotName==slotName) {
+        delete c.method;
+        delete c.slotName;
+        delete c.receiverName;
+        delete c.receiver;
+        return false;
+      } else {
+        return true;
       }
+    });
+    metaSignals[senderName][signalName] = removed;
+    if(!removed.length) {
+      delete metaSignals[senderName][signalName];
+      if(!Object.keys(metaSignals[senderName]).length) delete metaSignals[senderName];
+    }
+    else {
+      if(!Object.keys(metaSignals[senderName]).length) delete metaSignals[senderName];
     }
   }
 
-  var connectInner = (sender,signalName,receiver,slotName) => {
-    if(!sender || !sender['constructor']) throw 'No sender';
-    if(!receiver || !receiver['constructor']) throw 'No receiver';
+  replaceMethod(sender,signalName,receiver,slotName) {
     let senderName = sender.constructor.name;
     let receiverName = receiver.constructor.name;
-    if(typeof sender[signalName] != 'function') throw 'No signal';
-    if(typeof receiver[slotName] != 'function') throw 'No slot';
-
-    let metaObject = sender['metaObject'] = sender['metaObject'] || {};
-    metaObject[senderName] = metaObject[senderName] || [];
-    let [i,j] = insertIndex(sender,signalName,receiver,slotName);
-    console.log([i,j]);
-    metaObject[senderName][i] = metaObject[senderName][i] || [];
-    metaObject[senderName][i]['sender'] = senderName;
-
-    metaObject[senderName][i][signalName] = metaObject[senderName][i][signalName] || [];
-    metaObject[senderName][i][signalName].push((...args)=>receiver[slotName].call(receiver,...args));
-    metaObject[senderName][i][signalName][j]['receiver'] = receiverName;
-
+    let metaSignals = sender['metaSignals'];
     let method = sender[signalName]['signal'] || sender[signalName];
-    sender[signalName] = (...args) => {
-      method.call(sender,...args);
-      metaObject[senderName][i][signalName].map((m)=>m.call(receiver,...args));
-    };
-    sender[signalName]['signal'] = method;
-  };
 
-  var self = {
-    connect(sender,signalName,receiver,slotName) {
-      connectInner(sender,signalName,receiver,slotName);
-    },
-
-    disconnect(sender,signalName,receiver,slotName) {
-      disconnectInner(sender,signalName,receiver,slotName);
+    if(metaSignals[senderName]&&metaSignals[senderName][signalName]&&metaSignals[senderName][signalName].length) {
+      sender[signalName] = function (...args) {
+        let ret = method.call(sender,...args)
+        metaSignals[senderName][signalName].forEach(function(r) {
+          try {
+            if(r&&r.method) r.method.call(receiver,...args);
+            else {
+              console.log('Error connect(',sender,signalName,receiver,slotName,')');
+            }
+          } catch(e) {
+            console.log('Exception',e.message,e.stack);
+          }
+        });
+        return ret;
+      };
+      sender[signalName]['signal'] = method;
+    } else {
+      sender[signalName] = sender[signalName]['signal'] || sender[signalName];
     }
-  };
+  }
 
-  return self;
-})();
+  connect(sender,signalName,receiver,slotName) {
+    this.checkArgs(sender,signalName,receiver,slotName);
+    this.init(sender,signalName,receiver,slotName);
+    this.replaceMethod(sender,signalName,receiver,slotName);
+    console.log('[DEBUG]',`connect(${sender.constructor.name},${signalName},${receiver.constructor.name},${slotName})`);
+  }
 
-module.exports = MetaObject;
+  disconnect(sender,signalName,receiver,slotName) {
+    this.checkArgs(sender,signalName,receiver,slotName);
+    this.deinit(sender,signalName,receiver,slotName);
+    this.replaceMethod(sender,signalName,receiver,slotName);
+    console.log('[DEBUG]',`disconnect(${sender.constructor.name},${signalName},${receiver.constructor.name},${slotName})`);
+  }
+}
